e0828e123996ed9834ae61fa9156477729eec88d
LouiseHjorth <Lhj011200@icloud.com>
2024-09-19 14:19:27 +0000

    removed a file

--- test_packages:R.R
+++
@@ -1,8 +0,0 @@
-library(Rpostgres)
-<<<<<<< HEAD
-library(tidyverse) # often used in data science
-
-=======
-library(tidyverse) # load many packages
->>>>>>> 9ef0360a21681a37103d02b81e2e9c518127588e
-
